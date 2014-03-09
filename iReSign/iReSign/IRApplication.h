bcae49d030eed9dde5f27821c2177541987ac59a
Jochen Pfeiffer <jochen.pfeiffer@mobileeventguide.de>
2014-03-09 18:14:33 +0100

    enabled copy/past/select in text fields

---
+++ iReSign/iReSign/IRApplication.h
@@ -0,0 +1,13 @@
+//
+//  IRApplication.h
+//  iReSign
+//
+//  Created by Jochen Pfeiffer on 09.03.14.
+//  Copyright (c) 2014 nil. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface IRApplication : NSApplication
+
+@end
